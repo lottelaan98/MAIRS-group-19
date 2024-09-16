012e1010fa27f5af5f779a94eba82dac90790104
CerineExists <c.c.touber@students.uu.nl>
2024-09-16 22:25:56 +0200

    class ipv word

--- Baseline_agent_1.py
+++ Baseline_agent_1.py
@@ -21,17 +21,17 @@ df = df.drop(columns=[0])
 class baseline1:
     def __init__(self, dataset):
         self.data = dataset 
-        self.most_frequent_word = None
+        self.most_frequent_class = None
         self.find_most_frequent_dialog_act()
     
     def find_most_frequent_dialog_act(self):
         dataset = self.data
         first_dialog_act = dataset.iloc[:, 0].apply(lambda sentence: sentence.split()[0] if isinstance(sentence, str) else None)
         word_counts = Counter(first_dialog_act)
-        self.most_frequent_word = word_counts.most_common(1)[0][0]
+        self.most_frequent_class = word_counts.most_common(1)[0][0]
     
     def classify(self, sentence):
-        return self.most_frequent_word
+        return self.most_frequent_class
     
 classifier = baseline1(dataset)
-print('majority class = ', classifier.most_frequent_word)
+print('majority class = ', classifier.most_frequent_class)
